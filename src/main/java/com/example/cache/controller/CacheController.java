dad3419631abe81596487b83b4c51c0e6f492d9d
Arpit Agrawal <agrawal.arpit_88@yahoo.com>
2023-07-01 20:13:14 +0530

    Creates dao with a hashmap based implementation

--- src/main/java/com/example/cache/controller/CacheController.java
+++ src/main/java/com/example/cache/controller/CacheController.java
@@ -1,27 +1,30 @@
 package com.example.cache.controller;
 
-import java.util.HashMap;
 import java.util.Map;
 
+import com.example.cache.dao.CacheDao;
+import com.example.cache.dao.impl.HashMapCacheDao;
 import com.example.cache.dto.KeyValuePair;
 import com.example.cache.exception.KeyNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CacheController {
-    private HashMap<String, String> map = new HashMap<>();
+
+    @Autowired
+    private CacheDao cacheDao;
 
     @PostMapping("/put")
     @ResponseStatus(HttpStatus.CREATED)
     public void put(@RequestBody KeyValuePair p){
-        map.put(p.getKey(), p.getValue());
+        cacheDao.put(p.getKey(), p.getValue());
     }
 
     @GetMapping("/get")
     public Map<String, String> get(@RequestParam String key) throws KeyNotFoundException {
-        if(map.containsKey(key)) return Map.of(key, map.get(key));
-        throw new KeyNotFoundException(key);
+        return Map.of(key, cacheDao.get(key));
     }
 
     @ExceptionHandler(KeyNotFoundException.class)
