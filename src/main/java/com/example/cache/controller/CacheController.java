ca3c358fbf0cceebac94e913f658d6c98241d35a
Arpit Agrawal <meetmickeymoon@gmail.com>
2025-10-20 21:06:57 +0530

    Remove redundant autowired annotation

--- src/main/java/com/example/cache/controller/CacheController.java
+++ src/main/java/com/example/cache/controller/CacheController.java
@@ -3,18 +3,20 @@ package com.example.cache.controller;
 import java.util.Map;
 
 import com.example.cache.dao.CacheDao;
-import com.example.cache.dao.impl.HashMapCacheDao;
 import com.example.cache.dto.KeyValuePair;
 import com.example.cache.exception.KeyNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CacheController {
 
-    @Autowired
-    private CacheDao cacheDao;
+    private final CacheDao cacheDao;
+
+    public CacheController(@Qualifier("hashMapCacheDao") CacheDao cacheDao) {
+        this.cacheDao = cacheDao;
+    }
 
     @PostMapping("/put")
     @ResponseStatus(HttpStatus.CREATED)
